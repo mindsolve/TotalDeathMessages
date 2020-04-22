38eb60b32d7991160c79d3a78de08c45ba4c0be5
Felix <together+github@felix-ern.de>
2020-04-22 23:08:37 +0200

    Added weapon info to kill message

--- src/main/java/eu/fx3/plugins/totaldeathmessages/totaldeathmessages/EntityDeathListener.java
+++ src/main/java/eu/fx3/plugins/totaldeathmessages/totaldeathmessages/EntityDeathListener.java
@@ -1,18 +1,31 @@
 package eu.fx3.plugins.totaldeathmessages.totaldeathmessages;
 
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.apache.commons.lang.WordUtils;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageByBlockEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
-
+import org.bukkit.inventory.EntityEquipment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionType;
 
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+
+import static net.md_5.bungee.api.ChatColor.*;
 
 public class EntityDeathListener implements org.bukkit.event.Listener {
     JavaPlugin plugin = JavaPlugin.getPlugin(TotalDeathMessages.class);
@@ -45,12 +58,8 @@ public class EntityDeathListener implements org.bukkit.event.Listener {
             }
         }
 
-        if (plugin.getConfig().contains("ignore-worlds")) {
-            for (String worldName : plugin.getConfig().getStringList("ignore-worlds")) {
-                if (deadEntity.getWorld().getName().equals(worldName)) {
-                    return;
-                }
-            }
+        if (isWorldIgnored(deadEntity.getWorld().getName())) {
+            return;
         }
 
         if (plugin.getConfig().contains("ignore-entities")) {
@@ -112,38 +121,157 @@ public class EntityDeathListener implements org.bukkit.event.Listener {
         String originalEntityName = deadEntity.getType().getName().replace("_", " ");
         originalEntityName = WordUtils.capitalize(originalEntityName);
 
+        /*
+        TODO:
+            New:
+            $Player killed $Entity [$extra]
+            Old:
+            $Entity was killed by $Player [using $extra]
+         */
 
-        String deathMessage = "";
+
+        ComponentBuilder deathMessage = new ComponentBuilder().color(DARK_GRAY);
         if (hasCustomName) {
-            deathMessage = ChatColor.GOLD + killedEntityName + ChatColor.DARK_GRAY +
-                    " (that poor " + ChatColor.BLUE + originalEntityName + ChatColor.DARK_GRAY + ")";
+            deathMessage.append(killedEntityName).color(GOLD);
+            deathMessage.append(" (that poor ").color(DARK_GRAY).append(originalEntityName).color(BLUE).append(")").color(DARK_GRAY);
         } else {
             // Attach correct article
             String article = "A" + (killedEntityName.matches("^[AEIOU].*") ? "n " : " ");
-            deathMessage = ChatColor.DARK_GRAY + article + ChatColor.BLUE + killedEntityName + ChatColor.DARK_GRAY;
+            deathMessage.append(article).color(DARK_GRAY).append(killedEntityName).color(BLUE);
         }
 
+        // Add info for pets (owner), if applicable
         if (deadEntity instanceof Tameable) {
-            Tameable deadTameableEntity = (Tameable) deadEntity;
-            if (deadTameableEntity.isTamed()) {
-                if (!killerPlayer.getDisplayName().equals(deadTameableEntity.getOwner().getName())) {
-                    deathMessage += ", " + ChatColor.DARK_PURPLE + deadTameableEntity.getOwner().getName() + ChatColor.DARK_GRAY + "s pet,";
-                } else {
-                    deathMessage += ", the killers pet,";
-                }
-            }
+            deathMessage.append(getPetTextComponent(deadEntity).create());
         }
 
-        deathMessage += " was killed by player " + ChatColor.DARK_PURPLE + killerPlayer.getDisplayName() + ChatColor.RESET;
+        deathMessage.append(" was killed by player ").color(DARK_GRAY).append(killerPlayer.getDisplayName()).color(DARK_PURPLE);
+
+        if (deadEntity.getLastDamageCause() instanceof EntityDamageByEntityEvent) {
+            Entity damager = ((EntityDamageByEntityEvent) deadEntity.getLastDamageCause()).getDamager();
+
+            if (damager instanceof Projectile) {
+//                deathMessage.append(" with Projectile ").color(DARK_GRAY);
+                deathMessage.append(" ").color(DARK_GRAY);
+
+                if (damager instanceof Arrow) {
+                    Arrow arrowDamager = (Arrow) damager;
+
+                    if (arrowDamager.getBasePotionData().getType() != PotionType.UNCRAFTABLE && arrowDamager.getCustomEffects().isEmpty()) {
+                        deathMessage.append("Tipped Arrow with effect " + WordUtils.capitalizeFully(arrowDamager.getBasePotionData().getType().toString().replaceAll("_"," ")));
+                    } else {
+                        deathMessage.append("Arrow");
+                    }
+
+                } else if (damager instanceof ThrowableProjectile) {
+                    ThrowableProjectile throwableDamager = (ThrowableProjectile) damager;
+                    deathMessage.append("by throwing his ");
+                    TextComponent killerWeaponComponent = TotalDeathMessages.getInstance().getNmsItem().itemToTextComponent(throwableDamager.getItem());
+                    deathMessage.append(killerWeaponComponent);
+
+                } else if (damager instanceof ThrownPotion) {
+                    ThrownPotion potionDamager = (ThrownPotion) damager;
+                    ItemMeta potionItemMeta = potionDamager.getItem().getItemMeta();
+
+                    deathMessage.append("by throwing a ");
+
+                    PotionMeta potionMeta = (PotionMeta) potionItemMeta;
+                    ItemStack item = potionDamager.getItem();
+
+                    assert potionMeta != null;
+                    if (potionMeta.getBasePotionData().getType().equals(PotionType.INSTANT_DAMAGE)) {
+                        deathMessage.append("Potion of ").color(DARK_GRAY).append("Harming " +
+                                (potionMeta.getBasePotionData().isUpgraded() ? "II" : "I")).color(AQUA);
+                    } else {
+                        // This should not be possible, except (maybe) for cheating a whithering potion
+                        deathMessage.append(TotalDeathMessages.getInstance().getNmsItem().itemToTextComponent(item));
+                    }
+                } else {
+                    Projectile projectileDamager = (Projectile) damager;
+
+                    deathMessage.append("Projectile ");
+                    deathMessage.append(projectileDamager.getName());
+                    if (projectileDamager.getCustomName() != null) {
+                        deathMessage.append(" \"" + projectileDamager.getCustomName() + "\"");
+                    }
+                }
+            } else if (damager instanceof Player) {
+                // Keine Todeswaffe kann in der linken (offhand) getragen werden und töten, außer Bogen.
+                // Bogen wird allerdings durch Projectile -> Arrow abgedeckt
+
+                deathMessage.append(" by hitting it repeatedly with his ").color(DARK_GRAY);
+
+                EntityEquipment killerEquipment = killerPlayer.getEquipment();
+                if (killerEquipment != null) {
+                    ItemStack killerWeapon = killerEquipment.getItemInMainHand();
+                    TextComponent killerWeaponComponent = TotalDeathMessages.getInstance().getNmsItem().itemToTextComponent(killerWeapon);
+                    deathMessage.append(killerWeaponComponent);
+                } else {
+                    deathMessage.append("[Error: killerEquipment is null!]");
+                }
+
+            }
+
+        } else if (deadEntity.getLastDamageCause() instanceof EntityDamageByBlockEvent) {
+            deathMessage.append(" (How the heck did you do this? Event (block): " + ((EntityDamageByBlockEvent) deadEntity.getLastDamageCause()).getDamager().getType() + ")").color(DARK_RED);
+
+        } else if (deadEntity.getLastDamageCause().getCause() == EntityDamageEvent.DamageCause.MAGIC) {
+            // Might be an Arrow of Harming (I / II). Either we assume that here, or we ignore it (because we cant check: time of check vs time of arrow firing)
+
+            deathMessage.append(" using ").color(DARK_GRAY)
+                    .append("magic").color(AQUA)
+                    .append(" (").color(DARK_GRAY)
+                    .append("maybe Arrow of Harming?").italic(true)
+                    .append(")").reset().color(DARK_GRAY);
+
+        } else if (deadEntity.getLastDamageCause().getCause() == EntityDamageEvent.DamageCause.FIRE_TICK) {
+            // If cause == FIRE_TICK && getKiller == Player, this is cased by Fire* on Playerweapon
+            deathMessage.append(" using ").color(DARK_GRAY).append("Fire").color(AQUA).append("").color(DARK_GRAY);
+        } else {
+            deathMessage.append(" (How the heck did you do this? Event (other): " + deadEntity.getLastDamageCause().getCause() + ")").color(DARK_RED);
+        }
+
 
         // Add "Killing Spree" info
         if (currentKillStat.spreeKillCount > 1) {
-            deathMessage += ChatColor.RED + " (killing spree x" + currentKillStat.spreeKillCount + ")!!";
+            deathMessage.append(" (killing spree x" + currentKillStat.spreeKillCount + ")!!").color(RED);
         }
 
-        deathMessage += "!";
+        deathMessage.append("!");
 
-        Bukkit.broadcastMessage(deathMessage);
+        TotalDeathMessages.getInstance().getLogger().info(BaseComponent.toLegacyText(deathMessage.create()));
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            player.spigot().sendMessage(deathMessage.create());
+        }
 
+    }
+
+
+    private boolean isWorldIgnored(String eventWorldName) {
+        if (plugin.getConfig().contains("ignore-worlds")) {
+            for (String worldName : plugin.getConfig().getStringList("ignore-worlds")) {
+                if (eventWorldName.equals(worldName)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private ComponentBuilder getPetTextComponent(LivingEntity deadEntity) {
+        Player killerPlayer = deadEntity.getKiller();
+        ComponentBuilder thisPart = new ComponentBuilder("").color(DARK_GRAY);
+
+        Tameable deadTameableEntity = (Tameable) deadEntity;
+        if (deadTameableEntity.isTamed()) {
+            assert killerPlayer != null;
+            if (!killerPlayer.getName().equals(deadTameableEntity.getOwner().getName())) {
+                thisPart.append(", ").append(deadTameableEntity.getOwner().getName()).color(DARK_PURPLE).append("s pet,").color(DARK_GRAY);
+            } else {
+                thisPart.append(", the killers pet,").color(DARK_GRAY);
+            }
+        }
+
+        return thisPart;
     }
 }
--- src/main/java/eu/fx3/plugins/totaldeathmessages/totaldeathmessages/TotalDeathMessages.java
+++ src/main/java/eu/fx3/plugins/totaldeathmessages/totaldeathmessages/TotalDeathMessages.java
@@ -1,16 +1,29 @@
 package eu.fx3.plugins.totaldeathmessages.totaldeathmessages;
 
+import eu.fx3.plugins.totaldeathmessages.utils.NMSItem;
+import net.md_5.bungee.api.chat.*;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 
 public final class TotalDeathMessages extends JavaPlugin {
     FileWatcher configWatcher;
+    private static TotalDeathMessages instance;
+    private NMSItem nmsItem;
 
     @Override
     public void onEnable() {
+        // Save object reference
+        instance = this;
+
+        // Create NSMItem object
+        nmsItem = new NMSItem();
+
         // Register EventListener
         getServer().getPluginManager().registerEvents(new EntityDeathListener(), this);
 
@@ -31,6 +44,8 @@ public final class TotalDeathMessages extends JavaPlugin {
         // Log success
         getLogger().info(ChatColor.GREEN + "Plugin erfolgreich initialisiert!");
 
+
+
     }
 
     @Override
@@ -42,6 +57,14 @@ public final class TotalDeathMessages extends JavaPlugin {
     protected void updateConfig() {
         getLogger().info("Reloaded config.");
         reloadConfig();
+    }
+
+    public static TotalDeathMessages getInstance() {
+        return instance;
+    }
+
+    public NMSItem getNmsItem() {
+        return this.nmsItem;
     }
 
 }
