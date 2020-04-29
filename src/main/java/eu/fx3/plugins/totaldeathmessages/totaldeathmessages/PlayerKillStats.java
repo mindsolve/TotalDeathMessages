06a7a325cad780dd13c0e3b68e3fa6442ea2ffdc
Felix <together+github@felix-ern.de>
2020-04-29 22:58:46 +0200

    PlayerKillStats: Added field playerUUID (unused at the moment)

--- src/main/java/eu/fx3/plugins/totaldeathmessages/totaldeathmessages/PlayerKillStats.java
+++ src/main/java/eu/fx3/plugins/totaldeathmessages/totaldeathmessages/PlayerKillStats.java
@@ -1,8 +1,11 @@
 package eu.fx3.plugins.totaldeathmessages.totaldeathmessages;
 
+import java.util.UUID;
+
 public class PlayerKillStats {
     public long lastKillTime = 0;
     public String playerName;
+    public UUID playerUUID;
     public int spreeKillCount = 0;
     public int totalKillCount = 0;
 }
