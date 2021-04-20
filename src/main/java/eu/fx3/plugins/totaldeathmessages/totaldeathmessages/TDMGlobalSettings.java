24c9a3528e736fba4a19fd4855d2a96b3e05f7f3
Felix <together+github@felix-ern.de>
2021-04-20 14:06:02 +0200

    Replaced Trident launch tracking with HashMap, removed TridentLaunch class

--- src/main/java/eu/fx3/plugins/totaldeathmessages/totaldeathmessages/TDMGlobalSettings.java
+++ src/main/java/eu/fx3/plugins/totaldeathmessages/totaldeathmessages/TDMGlobalSettings.java
@@ -2,32 +2,22 @@ package eu.fx3.plugins.totaldeathmessages.totaldeathmessages;
 
 // TODO: Add JavaDoc
 
-import eu.fx3.plugins.totaldeathmessages.settingutils.TridentLaunch;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 import java.util.UUID;
 
 public class TDMGlobalSettings {
 
-    public List<TridentLaunch> tridentLaunches = new ArrayList<TridentLaunch>();
+    public HashMap<UUID, ItemStack> tridentLaunchesNew = new HashMap<>();
 
     public void registerTridentLaunch(UUID playerUUID, ItemStack launchedTrident) {
-        // Remove all previous launches
-        tridentLaunches.removeIf(launch -> playerUUID.equals(launch.launchPlayerUUID));
-
-        // Add current launch
-        tridentLaunches.add(new TridentLaunch(playerUUID, launchedTrident));
+        // Put launch in HashMap, overwriting previous launch (if applicable)
+        tridentLaunchesNew.put(playerUUID, launchedTrident);
     }
 
     public ItemStack getLastThrownTrident(UUID playerUUID) {
-        for (TridentLaunch launch : tridentLaunches) {
-            if (launch.launchPlayerUUID.equals(playerUUID)) {
-                return launch.launchedTridentItem;
-            }
-        }
-        return null;
+        return tridentLaunchesNew.get(playerUUID);
     }
 }
 
