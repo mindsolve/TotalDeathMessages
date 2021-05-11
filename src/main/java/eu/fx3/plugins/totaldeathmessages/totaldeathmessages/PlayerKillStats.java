5860d36f0d5505a963c7c48d99b3137caa091446
Felix <together+github@felix-ern.de>
2021-05-11 13:29:33 +0200

    PlayerKillStats: Added JavaDoc

--- src/main/java/eu/fx3/plugins/totaldeathmessages/totaldeathmessages/PlayerKillStats.java
+++ src/main/java/eu/fx3/plugins/totaldeathmessages/totaldeathmessages/PlayerKillStats.java
@@ -1,9 +1,24 @@
 package eu.fx3.plugins.totaldeathmessages.totaldeathmessages;
 
-// TODO: JavaDoc properties
+import java.time.Instant;
 
-public class PlayerKillStats {
+/**
+ * Dataclass to store kill statistics for a specific player
+ */
+class PlayerKillStats {
+    /**
+     * Last player kill as epoch timestamp (e.g. from {@link Instant#getEpochSecond()})
+     */
     public long lastKillTime = 0;
+
+    /**
+     * Kill count of the current killing spree
+     */
     public int spreeKillCount = 0;
+
+    /**
+     * Total kill count of a player.
+     * Currently unused.
+     */
     public int totalKillCount = 0;
 }
