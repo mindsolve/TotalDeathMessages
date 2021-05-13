8aac01d50786c96c50cddc8f78a426d84328be2e
Felix <together+github@felix-ern.de>
2021-05-13 15:11:26 +0200

    PlayerMessageSetting: Added JavaDoc

--- src/main/java/eu/fx3/plugins/totaldeathmessages/settingutils/PlayerMessageSetting.java
+++ src/main/java/eu/fx3/plugins/totaldeathmessages/settingutils/PlayerMessageSetting.java
@@ -1,7 +1,23 @@
 package eu.fx3.plugins.totaldeathmessages.settingutils;
 
+/**
+ * Enum for the three messaging levels players can choose.
+ * Used in the config file ("playerconfig") section and internally.
+ */
 public enum PlayerMessageSetting {
+    /**
+     * Player wants all messages, including all killing spree messages.
+     */
     ALL_MESSAGES,
+
+    /**
+     * Player wants no messages.
+     */
     NO_MESSAGES,
+
+    /**
+     * Player wants single mobdeath messages, but no "spammy" killing spree messages.
+     * Instead, killing spree ending will result in a single summary message.
+     */
     FEWER_MESSAGES
 }
