f9d63bb91e1861a8a5f3b7675f30f8fcb5eeb27e
Felix <together+github@felix-ern.de>
2021-05-18 20:57:10 +0200

    ChatStyles: Added private constructor, added stub documentation

--- src/main/java/eu/fx3/plugins/totaldeathmessages/totaldeathmessages/ChatStyles.java
+++ src/main/java/eu/fx3/plugins/totaldeathmessages/totaldeathmessages/ChatStyles.java
@@ -3,6 +3,13 @@ package eu.fx3.plugins.totaldeathmessages.totaldeathmessages;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.Style;
 
+/**
+ * Static utility class for Adventure chat styles
+ */
 public class ChatStyles {
+    private ChatStyles() {
+        throw new IllegalStateException("This class should not be instantiated.");
+    }
+
     public static final Style BASE_STYLE = Style.style(NamedTextColor.DARK_GRAY);
 }
